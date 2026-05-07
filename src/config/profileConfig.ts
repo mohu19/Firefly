36e01c3410239593dfc9d3cc9108dd98765fbc01
mohu19 <99907151+mohu19@users.noreply.github.com>
2026-05-08 01:51:32 +0800

    Update profileConfig.ts

--- src/config/profileConfig.ts
+++ src/config/profileConfig.ts
@@ -12,17 +12,17 @@ export const profileConfig: ProfileConfig = {
 	name: "墨狐",
 
 	// 个人签名
-	bio: "愿未来的人更加善良与智慧",
+	bio: "愿未来的人更加智慧与善良",
 
 	// 链接配置
 	// 已经预装的图标集：fa7-brands，fa7-regular，fa7-solid，material-symbols，simple-icons
 	// 访问https://icones.js.org/ 获取图标代码，
 	// 如果想使用尚未包含相应的图标集，则需要安装它
 	// `pnpm add @iconify-json/<icon-set-name>`
-	/* showName: true 时显示图标和名称，false 时只显示图标
+	/* showName: true 时显示图标和名称，false 时只显示图标 
 	
 	
-	/*
+	
 	links: [
 		{
 			name: "qq",
@@ -47,6 +47,8 @@ export const profileConfig: ProfileConfig = {
 			icon: "fa7-solid:rss",
 			url: "/rss/",
 			showName: false,
-		},
-	],
+			},
+		],
 	};
+
+	*/
