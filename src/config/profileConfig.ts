9b728db83e15167b6211b9982f376e93f2a609eb
mohu19 <99907151+mohu19@users.noreply.github.com>
2026-05-08 01:45:56 +0800

    Update profileConfig.ts

--- src/config/profileConfig.ts
+++ src/config/profileConfig.ts
@@ -6,20 +6,23 @@ export const profileConfig: ProfileConfig = {
 	// 1. public 目录（以 "/" 开头，不优化）："/assets/images/avatar.webp"
 	// 2. src 目录（不以 "/" 开头，自动优化但会增加构建时间，推荐）："assets/images/avatar.webp"
 	// 3. 远程 URL："https://example.com/avatar.jpg"
-	avatar: "assets/images/avatar.avif",
+	avatar: "assets/images/headphoto.png",
 
 	// 名字
-	name: "Firefly",
+	name: "墨狐",
 
 	// 个人签名
-	bio: "Hello, I'm Firefly.",
+	bio: "愿未来的人更加善良与智慧",
 
 	// 链接配置
 	// 已经预装的图标集：fa7-brands，fa7-regular，fa7-solid，material-symbols，simple-icons
 	// 访问https://icones.js.org/ 获取图标代码，
 	// 如果想使用尚未包含相应的图标集，则需要安装它
 	// `pnpm add @iconify-json/<icon-set-name>`
-	// showName: true 时显示图标和名称，false 时只显示图标
+	/* showName: true 时显示图标和名称，false 时只显示图标
+	
+	
+	/*
 	links: [
 		{
 			name: "qq",
@@ -46,4 +49,4 @@ export const profileConfig: ProfileConfig = {
 			showName: false,
 		},
 	],
-};
+	};
