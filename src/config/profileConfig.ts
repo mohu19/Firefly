f7c1d01923d43644b4da5a9056c5efa6ae7817ba
mohu19 <99907151+mohu19@users.noreply.github.com>
2026-05-08 13:29:27 +0800

    Update profileConfig.ts

--- src/config/profileConfig.ts
+++ src/config/profileConfig.ts
@@ -19,7 +19,7 @@ export const profileConfig: ProfileConfig = {
 	// 访问https://icones.js.org/ 获取图标代码，
 	// 如果想使用尚未包含相应的图标集，则需要安装它
 	// `pnpm add @iconify-json/<icon-set-name>`
-	/*showName: true 时显示图标和名称，false 时只显示图标 
+	//showName: true 时显示图标和名称，false 时只显示图标 
 	
 	
 	
@@ -51,4 +51,3 @@ export const profileConfig: ProfileConfig = {
 		],
 	};
 
-*/
